0c50b2b9a8de79be57ad2c776b37bd0f1a7000c6
Primus Vekuh <primus.vekuh@gmail.com>
2023-02-22 12:41:06 -0500

    added docker build stage

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 app.get('/test', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from /test Node!';
+  const msg = 'Hello from Primus Learning and from /test Node!';
   res.send(getPage(msg));
 });
 
